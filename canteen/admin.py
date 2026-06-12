62ea9c63f2d0d9a289acfe112379b77db7e12fe3
nurlybekchel <nurlybekjj00@gmail.com>
2026-06-12 22:54:42 +0600

    Update admin.py

--- canteen/admin.py
+++ canteen/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, MenuItem, Order, OrderItem, Transaction
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Transaction)
