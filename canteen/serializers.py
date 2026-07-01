6b576c1dab7a6053e90665d5028137dd462142c1
Munar.Er1kM <munarsauyt@gmail.com>
2026-07-01 05:53:53 +0500

    Update serializers.py

--- canteen/serializers.py
+++ canteen/serializers.py
@@ -49,6 +49,27 @@ class TransactionSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, min_length=6)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role', 'balance']
+        fields = ['id', 'username', 'email', 'role', 'balance', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
